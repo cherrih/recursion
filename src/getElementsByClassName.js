c965790c20d0d9822afc1a0aad3a31598c5b9b38
cherrih <cherri.hartigan@gmail.com>
2018-10-16 11:03:49 -0700

    minimum requirements passing

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,36 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+// You should use:
+//document.body: Returns the <body> or <frameset> node of the current document, or null if no such element exists
+//element.childNodes: returns a live NodeList of child nodes of the given element where the first child node is assigned index 0
+//element.classList: returns a collection of the class attributes of the element
+
+//input = class to search for
+//output = array of nodes
+
+var getElementsByClassName = function(className) {
+  var elements = [];
+
+  //function that finds whether each element satisfies a condition (contains className) 
+  //add to result
+  var matchesClassName = function(element){
+    if (element.classList && element.classList.contains(className)){
+      elements.push(element);
+    }
+    if (element.childNodes){
+      element.childNodes.forEach(function(item){
+        matchesClassName(item);
+      })
+    }
+  }
+  //check if there are children nodes
+  //recursive call again
+  
+  matchesClassName(document.body);
+  return elements;
+
 };
+
+
+
